3f44186aaaf6550459c5250b0d8906f7e364c46c
noorsroor <noorsroor67@gmail.com>
2025-01-18 22:04:35 +0300

    update shop page,searsh bar

--- index.js
+++ index.js
@@ -36,3 +36,39 @@ let toSignUp = document.getElementById("signup-btn");
 toSignUp.addEventListener("click", ()=>{
     window.location.href="./login.html";
 });
+
+
+
+// Get modal elements
+const modal = document.getElementById('signupModal');
+const addToCartBtn = document.getElementById('addToCartBtn');
+const closeModal = document.getElementById('closeModal');
+const goToSignupBtn = document.getElementById('goToSignupBtn');
+
+
+
+// Show the modal when "Add to Cart" is clicked
+addToCartBtn.addEventListener('click', () => {
+  popUp();
+
+});
+
+function popUp(){
+  modal.style.display = 'block';
+  // Close the modal when the "X" button is clicked
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  // Redirect to signup page when "Go to Sign Up" button is clicked
+  goToSignupBtn.addEventListener('click', () => {
+    window.location.href = 'registration.html'; // Replace 'signup.html' with your signup page URL
+  });
+
+  // Close the modal if the user clicks outside the modal content
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
